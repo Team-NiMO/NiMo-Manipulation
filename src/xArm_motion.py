af830e603cfd4bb7a50b80cd4cf28d639eb3e502
shrudh-i <shrudhi.rs21@gmail.com>
2024-03-15 20:20:38 -0400

    changes to home and go2EM service files

--- src/xArm_motion.py
+++ src/xArm_motion.py
@@ -21,7 +21,6 @@ Services involved in this package:
 6. unhook
 '''
 
-
 class xArm_Motion():
     @classmethod
     def __init__(self, ip_addr):
@@ -47,9 +46,8 @@ class xArm_Motion():
     def home_pos(self):
         if VERBOSE:
             rospy.loginfo('Going to Home Position')
-        '''
+
         self.get_xArm_service = rospy.Service('home_pos', home_pos, )
-        '''
         self.arm.set_servo_angle(angle=[0, -90, 0, 0, 0, 0], is_radian=False, wait=True)
 
     @classmethod
@@ -65,20 +63,27 @@ class xArm_Motion():
         self.go2EM()
 
         print(f"---- going to cleaning nozzle ----")
-        ### TO DO: MODIFY THIS TO INCLUDE NEW CODE FROM AMIGA ###
+        ### TODO: MODIFY THIS TO INCLUDE NEW CODE FROM AMIGA ###
         self.arm.set_servo_angle(angle=[-133.6, 56.9, -53.4, 46, -85.6, 0], is_radian=False, wait=True)
-        ### TO DO: MODIFY THIS TO INCLUDE NEW CODE FROM AMIGA ###
+        ### TODO: MODIFY THIS TO INCLUDE NEW CODE FROM AMIGA ###
 
         print(f"---- going to calibration nozzles ----")
-        ### TO DO: MODIFY THIS TO INCLUDE NEW CODE FROM AMIGA ###
+        ### TODO: MODIFY THIS TO INCLUDE NEW CODE FROM AMIGA ###
         self.arm.set_servo_angle(angle=[-118.3, 60.2, -69.9, 62.8, -77.9, -13.2], is_radian=False, wait=True)
         self.arm.set_servo_angle(angle=[-110.4, 75.9, -107.8, 73, -74.5, -33.8], is_radian=False, wait=True)
-        ### TO DO: MODIFY THIS TO INCLUDE NEW CODE FROM AMIGA ###
+        ### TODO: MODIFY THIS TO INCLUDE NEW CODE FROM AMIGA ###
 
     @classmethod
     def go2corn(self):
         if VERBOSE:
             rospy.loginfo("Going to the cornstalk")
+        
+        # [x, y, z] of the cornstalk (IN THE WORLD FRAME)
+        corn_pose = self.get_stalk_pose()
+
+        #TODO: Include the code of the the xArm moving to the vicinity of the cornstalk
+        
+
 
     @classmethod
     def arc_motion(self):
@@ -94,3 +99,22 @@ class xArm_Motion():
     def unhook(self):
         if VERBOSE:
             rospy.loginfo("Going to unhook the cornstalk")
+
+    @classmethod
+    def get_stalk_pose(self):
+        if VERBOSE:
+            rospy.loginfo("Getting the pose of the cornstalk")
+
+        # Getting the pose of the cornstalk from the perception stack (using the get_stalk service)
+        rospy.wait_for_service('get_stalk')
+        get_stalk_service = rospy.ServiceProxy('get_stalk', GetStalk)
+
+        try:
+            s_pose = get_stalk_service(num_frames=1, timeout=30.0) #5 frames,20 sec
+        except rospy.ServiceException as exc:
+            print("Service did not process request: " + str(exc))
+
+        print(' ---- Got response from stalk detection:', s_pose.position, ' ----')
+
+        print("POSE IS", [s_pose.position.x, s_pose.position.y, s_pose.position.z])
+        return  np.array([s_pose.position.x, s_pose.position.y, s_pose.position.z])
